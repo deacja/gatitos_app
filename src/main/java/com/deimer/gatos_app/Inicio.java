a5f84d08f3f642763c4593ad8c5bb24b42e8c302
Deimer Acevedo <deimerace@gmail.com>
2021-11-07 18:18:37 -0500

    primer commit

---
+++ src/main/java/com/deimer/gatos_app/Inicio.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.deimer.gatos_app;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author deime
+ */
+public class Inicio {
+    
+    public static void main(String[] args) {
+        int opcion_menu = -1;
+        String[] botones = {"1. Ver gatos.", "2. Salir"};
+        
+        do {
+            //Menú principal
+            String opcion = (String) JOptionPane.showInputDialog(null, "Gatitos Java", "Menu Principal", JOptionPane.INFORMATION_MESSAGE, null, botones, botones[0]);
+            
+            //validamos que opcion selecciona el Usuario
+            for (int i=0; i<botones.length; i++){
+                if (opcion.equals(botones[i])){
+                    opcion_menu = i;
+                }
+            }
+            
+            switch (opcion_menu){
+                case 0:
+                    break;
+                default:
+                    break;
+            }
+            
+        } while (opcion_menu != 1);
+    }
+    
+}
---
+++ src/main/java/com/deimer/gatos_app/gatos.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.deimer.gatos_app;
+
+/**
+ *
+ * @author deime
+ */
+public class gatos {
+    
+    int is;
+    String url;
+    String apikey = "29ab3fcf-1ae1-455c-89cd-a0ef5af37d1c";
+    String image;
+
+    public int getIs() {
+        return is;
+    }
+
+    public void setIs(int is) {
+        this.is = is;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+    
+}
